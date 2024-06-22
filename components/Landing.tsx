fbe1307e95014c9ad10d99827761c37505e55dee
Marina Ananias <ananias.marina@gmail.com>
2024-06-22 01:30:22 -0300

    Feature/landing record player (#18)
    * Landing :Title Styled + Layout: NavBar Added
    
    * Landing: Added Record Player + Formated Section

--- components/Landing.tsx
+++ components/Landing.tsx
@@ -1,13 +1,37 @@
-import React from 'react';
-import styles from '../styles/landing.module.css';
+import React from "react";
+import Image from "next/image";
+import styles from "../styles/landing.module.css";
 
 export default function Landing() {
   return (
-    <section id="landing" className={styles.landing}>
+    <section id="landing" className={styles.subContainer}>
       <div className={styles.greetings}>
         <h3 className={styles.greet1}>Oi!</h3>
         <h3 className={styles.greet2}>(Hello!)</h3>
         <h3 className={styles.name}>I'm Marina</h3>
+      </div>
+      <div className={styles.recordPlayer}>
+        <Image
+          src="/images/vBase.svg"
+          alt="Record Player Base"
+          width={300}
+          height={300}
+          className={styles.vinylBase}
+        />
+        <Image
+          src="/images/vinyl.svg"
+          alt="Vinyl CD"
+          width={300}
+          height={300}
+          className={styles.vinyl}
+        />
+        <Image
+          src="/images/vNeedle.png"
+          alt="Vinyl Needle"
+          width={300}
+          height={300}
+          className={styles.vinylNeedle}
+        />
       </div>
     </section>
   );
