6476826993e837e01542f0953c6bacee266518f2
Marina Ananias <ananias.marina@gmail.com>
2024-06-22 01:51:57 -0300

    Feature/landing logo rotation (#32)
    * Landing :Title Styled + Layout: NavBar Added
    
    * Landing: Added Record Player + Formated Section
    
    * Landing: Added logo and Vinyl CD Rotation

--- components/Landing.tsx
+++ components/Landing.tsx
@@ -5,6 +5,15 @@ import styles from "../styles/landing.module.css";
 export default function Landing() {
   return (
     <section id="landing" className={styles.subContainer}>
+      <div className={styles.logoContainer}>
+        <Image
+          src="/images/logoShw.svg"
+          alt="Shadowed Logo"
+          width={50}
+          height={50}
+          className={styles.logo}
+        />
+      </div>
       <div className={styles.greetings}>
         <h3 className={styles.greet1}>Oi!</h3>
         <h3 className={styles.greet2}>(Hello!)</h3>
