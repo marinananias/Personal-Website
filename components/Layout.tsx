e51bfb6d3a1a6e0c3522f910ef8fc43a4f188ff6
marinananias <marina.ananias@willowtreeapps.com>
2024-06-22 00:24:04 -0300

    Landing :Title Styled + Layout: NavBar Added

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,14 +1,17 @@
-import React from 'react';
-import styles from '../styles/layout.module.css';
+import React from "react";
+import styles from "../styles/layout.module.css";
+import NavBar from "./NavBar";
 
-export const name = 'Marina Ananias';
+export const name = "Marina Ananias";
 
 export default function Layout({ children }) {
   return (
     <div className={styles.container}>
-      {/* Header */}
+      <header>
+        <NavBar />
+      </header>
       {children}
-      <footer className={styles.ft} >
+      <footer className={styles.ft}>
         <p>© Copyright 2024 Marina Ananias</p>
       </footer>
     </div>
