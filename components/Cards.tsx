418625b773bd45d7233facf912ae96c8cec01831
Marina Ananias <ananias.marina@gmail.com>
2024-06-24 11:05:19 -0300

    Abt txt title photo (#36)
    * Subcontainer background-color update
    
    * Updated About Title, Photo, Button

--- components/Cards.tsx
+++ components/Cards.tsx
@@ -1,34 +1,31 @@
-import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import useMediaQuery from '@mui/material/useMediaQuery';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import Typography from '@mui/material/Typography';
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import Typography from "@mui/material/Typography";
 
 export default function MediaCard() {
   const theme = createTheme({
     typography: {
-      fontFamily: [
-        'Lato',
-        '"Helvetica Neue"',
-        'Roboto',
-        'sans-serif'
-      ].join(','),
-    }
+      fontFamily: ["Lato", '"Helvetica Neue"', "Roboto", "sans-serif"].join(
+        ","
+      ),
+    },
   });
 
-  const matchesXsOnly = useMediaQuery(theme.breakpoints.only('xs'));
-  const matchesLgOnly = useMediaQuery(theme.breakpoints.only('lg'));
+  const matchesXsOnly = useMediaQuery(theme.breakpoints.only("xs"));
+  const matchesLgOnly = useMediaQuery(theme.breakpoints.only("lg"));
 
-  const cardMaxWidth = matchesXsOnly ? '100%' : matchesLgOnly ? 350 : 250;
+  const cardMaxWidth = matchesXsOnly ? "100%" : matchesLgOnly ? 350 : 250;
 
   return (
     <Card sx={{ maxWidth: cardMaxWidth }}>
       <CardMedia
-        sx={{ height: 'auto' }}
-        image="/static/images/cards/contemplative-reptile.jpg"
+        sx={{ height: "auto" }}
+        image="images/logo.jpeg"
         title="Project Title"
       />
       <CardContent>
@@ -39,9 +36,7 @@ export default function MediaCard() {
           Description
         </Typography>
       </CardContent>
-      <CardActions>
-        {/* GitHub Icon + Link */}
-      </CardActions>
+      <CardActions>{/* GitHub Icon + Link */}</CardActions>
     </Card>
   );
 }
