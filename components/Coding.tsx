3090be04f8542ffb936ced23f74a2e09b88bed6d
Marina Ananias <ananias.marina@gmail.com>
2024-06-24 18:40:57 -0300

    Feature/coding skills (#41)
    * Subcontainer background-color update
    
    * Updated About Title, Photo, Button
    
    * Updated and styled Cards and CardsGrid
    
    * Title of coding section formated
    
    * Added and formated "skills" on coding section

--- components/Coding.tsx
+++ components/Coding.tsx
@@ -10,6 +10,22 @@ export default function Coding() {
       className={`${utils.subContainers} ${styles.subContainer}`}
     >
       <h2 className={styles.title}>CODING</h2>
+      <div className={styles.skills}>
+        <div className={styles.skillsCol}>
+          <p>C</p>
+          <p>C++</p>
+          <p>Python</p>
+        </div>
+        <div className={styles.skillsCol}>
+          <p>Java</p>
+          <p>JavaScript</p>
+          <p>TypeScript</p>
+        </div>
+        <div className={styles.skillsCol}>
+          <p>CSS</p>
+          <p>HTML</p>
+        </div>
+      </div>
       {/* <CardsGrid></CardsGrid> */}
     </section>
   );
