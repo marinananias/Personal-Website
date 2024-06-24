c8b276a1a427e7b451a906c2ca4b09f842771865
Marina Ananias <ananias.marina@gmail.com>
2024-06-24 19:20:21 -0300

    Feature/music projects (#44)
    * Subcontainer background-color update
    
    * Updated About Title, Photo, Button
    
    * Updated and styled Cards and CardsGrid
    
    * Title of coding section formated
    
    * Added and formated "skills" on coding section
    
    * Added projects to the coding section and pictures for it and the music section
    
    * Formatted music section title
    
    * Added and formated projects in the music section

--- components/Music.tsx
+++ components/Music.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import CardsGrid from "./CardsGrid";
 import styles from "../styles/music.module.css";
 import utils from "../styles/utils.module.css";
+import Cards from "./Cards";
 
 export default function Music() {
   return (
@@ -12,6 +13,28 @@ export default function Music() {
       <div className={styles.titleBox}>
         <h2 className={styles.title1}>Making</h2>
         <h2 className={styles.title2}>MUSIC</h2>
+      </div>
+      <div className={styles.projects}>
+        <CardsGrid>
+          <Cards
+            title="Broken Smile"
+            image="/images/broken-smile.png"
+            link="https://github.com/marinananias/MIDI-Songwriter-Prototype"
+            section="music"
+          />
+          <Cards
+            title="Expectations"
+            image="/images/expectations.png"
+            link="https://github.com/marinananias/MIDI-Keyboard"
+            section="music"
+          />
+          <Cards
+            title="Luz"
+            image="/images/luz.png"
+            link="https://github.com/marinananias/MIDI-Keyboard"
+            section="music"
+          />
+        </CardsGrid>
       </div>
     </section>
   );
