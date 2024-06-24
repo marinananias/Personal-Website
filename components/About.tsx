1e3be268c7069968a16134635d87646f3b5954ea
marinananias <marina.ananias@willowtreeapps.com>
2024-06-24 11:04:08 -0300

    Updated About Title, Photo, Button

--- components/About.tsx
+++ components/About.tsx
@@ -9,24 +9,56 @@ export default function About() {
       id="about"
       className={`${utils.subContainers} ${styles.subContainer}`}
     >
-      <h2 className={`${utils.tit} ${styles.titAbt}`}>About Me</h2>
-      <div className={styles.abtContent}>
-        <Image
-          src="/images/profile.jpg"
-          sizes="100vw"
-          style={{
-            width: "40%",
-            height: "auto",
-            borderRadius: "0.5rem",
-          }}
-          width={500}
-          height={300}
-          alt="Marina Ananias"
-        />
-        <div className={styles.txtButton}>
-          <p> Text PlaceHolder </p>
-          <button> Button </button>
+      <div className={styles.txt}>
+        <p>
+          Originally from Maceió-AL, Brazil, I'm currently pursuing a Bachelor's
+          degree in{" "}
+          <span style={{ color: "#EC297B" }}>
+            Computer Science and Music with a minor in Neuroscience{" "}
+          </span>{" "}
+          at Grinnell College, Iowa. I'm fascinated by the intersection of these
+          fields and passionate about exploring interdisciplinary projects that
+          leverage the connections between them.
+        </p>
+        <br />
+        <p>
+          With every note played and code written, I embrace the pursuit of
+          knowledge, the joy of artistic expression, and the exploration of the
+          human mind. The people around me, alongside
+          <span style={{ color: "#EC297B" }}>
+            {" "}
+            courage, curiosity, and creativity
+          </span>
+          , are the main instruments that accompany me.
+        </p>
+        <br />
+        <p>
+          I love to hang out with my family and friends, embark on intergalactic
+          adventures in the Star Wars universe, sing Mamma Mia songs in the
+          shower, unravel mysteries with Shawn and Gus in Psych, and conquer
+          shrines in Zelda.
+        </p>
+      </div>
+      <div className={styles.button} role="button">
+        Resume
+      </div>
+      <div className={styles.titlePhoto}>
+        <div className={styles.titleBox}>
+          <div className={styles.title1}>
+            <h2>Abt.</h2>
+          </div>
+          <div className={styles.title2}>
+            <h2>Me</h2>
+          </div>
         </div>
+        <Image
+          className={styles.photo}
+          width={300}
+          height={300}
+          priority={true}
+          alt="Marina Ananias"
+          src="/images/profile.png"
+        />
       </div>
     </section>
   );
